60ed0684cf65b19b9a1e5a237b154bf566250711
Jacquese <jacqwhitson@gmail.com>
2023-05-29 09:16:04 -0400

    Initial Commit

---
+++ OffSeason3/OffSeason3App.swift
@@ -0,0 +1,17 @@
+//
+//  OffSeason3App.swift
+//  OffSeason3
+//
+//  Created by Jacquese Whitson  on 5/29/23.
+//
+
+import SwiftUI
+
+@main
+struct OffSeason3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
