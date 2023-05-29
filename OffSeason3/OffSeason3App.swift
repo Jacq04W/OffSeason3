01a9a296c87ac9938c4a67aaf3811d2c350ab706
Jacquese <jacqwhitson@gmail.com>
2023-05-29 09:26:16 -0400

    added configuration

--- OffSeason3/OffSeason3App.swift
+++ OffSeason3/OffSeason3App.swift
@@ -5,13 +5,36 @@
 //  Created by Jacquese Whitson  on 5/29/23.
 //
 
+
 import SwiftUI
+import Firebase
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
 
 @main
-struct OffSeason3App: App {
+struct Snacktacku_arApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    init(){
+        FirebaseApp.configure()
+    }
+//@StateObject var locationManager = LocationManager()
+//@StateObject var spotVm = SpotViewModel()
     var body: some Scene {
         WindowGroup {
             ContentView()
+//            LoginView()
+//                .environmentObject(spotVm)
+//                .environmentObject(locationManager)
+
+                 
         }
     }
 }
