7fe5f34620113e4f848f3ceed623a88de99acadc
Jacquese <jacqwhitson@gmail.com>
2023-06-07 16:27:39 -0400

    added Photo model

---
+++ OffSeason3/Models/Photo.swift
@@ -0,0 +1,27 @@
+//
+//  Photo.swift
+//  Snacktacku;ar
+//
+//  Created by Jacquese Whitson  on 5/13/23.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+
+struct Photo : Identifiable,Codable{
+    @DocumentID var id: String?
+    var imageURLString = ""
+    var description = ""
+    var reviewer = Auth.auth().currentUser?.email ?? ""
+    var postedOn = Date()
+    var dictionary : [String: Any]{
+        return[ "imageURLString" : imageURLString,"description" :  description,
+                "reviewer" : reviewer, "postedOn" : Timestamp(date: Date())]
+    }
+
+
+    
+}
+
