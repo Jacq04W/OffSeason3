319c841af1da3b954ee1745cef73f5a706f9a733
Jacquese Whitson <jacqwhitsonappledev@gmail.com>
2023-06-21 21:29:00 -0400

    jacq update

---
+++ OffSeason3/Views/ExplorListCard.swift
@@ -0,0 +1,62 @@
+//
+//  ExplorListCard.swift
+//  OffSeason3
+//
+//  Created by Jacquese Whitson on 6/21/23.
+//
+
+import SwiftUI
+import Firebase
+import FirebaseCore
+import FirebaseFirestoreSwift
+import SwiftUI
+import MapKit
+import PhotosUI
+import WeatherKit
+struct ExplorListCard: View {
+    @FirestoreQuery(collectionPath: "games") var games: [Game]
+    
+    // view models
+    @EnvironmentObject var locationVm : LocationManager
+    @EnvironmentObject var mapVm : MapViewModel
+    @EnvironmentObject var gameVm : GameViewModel
+    @EnvironmentObject private var weatherViewModel: WeatherViewModel
+   @State var game: Game
+    
+    var body: some View {
+        ZStack{
+            Image("cover2")
+                .resizable()
+                .cornerRadius(30)
+                .scaledToFit()
+            VStack(alignment: .leading){
+                Text("game.name")
+                .font(Font.custom("SportSpiritAf", size: 40))
+                .lineLimit(1)
+            }
+            .frame(width: 394,height: 70)
+            .background(.ultraThinMaterial)
+            .cornerRadius(40)
+            .offset(y:175)
+            
+            
+            
+           
+            
+            
+        }
+        .onTapGesture {
+            
+        }
+    }
+}
+
+struct ExplorListCard_Previews: PreviewProvider {
+    static var previews: some View {
+        ExplorListCard(game:Game())
+            .environmentObject(LocationManager())
+            .environmentObject(MapViewModel())
+            .environmentObject(GameViewModel())
+            .environmentObject(WeatherViewModel())
+    }
+}
